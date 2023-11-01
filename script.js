57e23e8e142e0ee2e6c00d4eb719d32631c99d14
samksksk <98775007+eusamk@users.noreply.github.com>
2023-11-01 17:32:36 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const videoPlayer = document.getElementById("videoPlayer");
+const changeVideoButton = document.getElementById("changeVideo");
+
+const videoFolder = "/"; // Pasta onde estão os vídeos
+const videoFiles = ["PiscadaSolidaria.mp4", "PiscadaCrista.mp4", "PiscadaCORINTHIANS.mp4", "ComboPiscadas.mp4", "PiscadaCuradoura.mp4", "PiscadaGringa.mp4", "PiscadaORGULHOSA.mp4", "PiscadaSULISTA.mp4","PiscadaLenta.mp4", "PiscadaMedicinal.mp4"]; // Lista de nomes dos vídeos
+
+let currentVideoIndex = 0;
+
+changeVideoButton.addEventListener("click", () => {
+    currentVideoIndex = (currentVideoIndex + 1) % videoFiles.length;
+    const nextVideo = videoFolder + videoFiles[currentVideoIndex];
+    videoPlayer.src = nextVideo;
+    videoPlayer.load();
+    videoPlayer.play();
+
+});
